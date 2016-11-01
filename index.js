aef5ad72bbf960de7e26378c86216d51d934c55e
isabel522 <perezisabel522@gmail.com>
2016-11-01 18:01:46 +0000

    Neeew message.

---
+++ index.js
@@ -0,0 +1,15 @@
+// write your code below!
+var happyHolidays = function(){ return 'Happy holidays!'
+                   };
+
+var happyHolidaysTo = function(){
+  return 'Happy holidays, you!'
+};
+
+var happyHolidayTo = function(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+};
+
+var holidayCountdown = function(holiday, days){
+return `It's ${days} days until ${holiday}!`;
+};
